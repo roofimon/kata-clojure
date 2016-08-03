a98ac7fa621594de0ffd50a8682ee5404bebd8fa
Twin Panichsombat <roofimon@gmail.com>
2016-08-03 15:43:43 +0700

    Move logic to main folder

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,7 +1,14 @@
 (ns clojure-noob.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn modulus3? [number] (zero? (mod number 3)))
+
+(defn modulus5? [number] (zero? (mod number 5)))
+
+(defn fizzbuzz
+  [number]
+    (cond
+      (and (modulus3? number) (modulus5? number) ) "fizzbuzz"
+      (modulus3? number) "fizz"
+      (modulus5? number) "buzz"
+      :else (str number)))
--- test/clojure_noob/core_test.clj
+++ test/clojure_noob/core_test.clj
@@ -1,17 +1,6 @@
 (ns clojure-noob.core-test
   (:require [clojure.test :refer :all] [clojure-noob.core :refer :all]))
 
-(defn modulus3? [number] (zero? (mod number 3)))
-
-(defn modulus5? [number] (zero? (mod number 5)))
-
-(defn fizzbuzz
-  [number]
-    (cond
-      (and (modulus3? number) (modulus5? number) ) "fizzbuzz"
-      (modulus3? number) "fizz"
-      (modulus5? number) "buzz"
-      :else (str number)))
 
 (deftest cuount-1-should-be-1
   (is (= "1" (fizzbuzz 1))))
