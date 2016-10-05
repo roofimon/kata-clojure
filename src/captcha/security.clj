26a4d6030e88122407b1dfef943b2266c1149d23
Twin Panichsombat <roofimon@gmail.com>
2016-10-05 17:28:41 +0800

    Fix typo

--- src/captcha/security.clj
+++ src/captcha/security.clj
@@ -1,34 +1,29 @@
 (ns captcha.security)
-
 (def m ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 
 (defn string [captcha]
   (str (get captcha :left) " " (get captcha :operator) " " (get captcha :right)))
 
-(defn second? [pattern] (= pattern 2))
+(defn string-integer? [pattern] (= pattern 2))
 
 (defn string-operand [index] (get m (- index 1)) )
 
 (def integer-operand (fn [index] (str index)))
 
-(defn assign-left-operand
-  [pattern left]
-  (if (second? pattern)
+(defn assign-left-operand [pattern left]
+  (if (string-integer? pattern)
     (string-operand left)
     (integer-operand left) ))
 
-(defn assign-right-operand
-  [pattern right]
-  (if (second? pattern)
+(defn assign-right-operand [pattern right]
+  (if (string-integer? pattern)
     (integer-operand right)
     (string-operand right) ))
 
-(defn assign-operator
-  [operator]
+(defn assign-operator [operator]
   (get ["+" "*" "-"] (- operator 1)))
 
-(def captcha
-    (fn [pattern left operator right]
+(def captcha (fn [pattern left operator right]
       {:left (assign-left-operand pattern left)
         :operator (assign-operator operator)
         :right (assign-right-operand pattern right)}))
