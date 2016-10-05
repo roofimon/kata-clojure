eee883991685f84e71799cac00752a99bc3ce9c4
Twin Panichsombat <roofimon@gmail.com>
2016-10-05 11:21:08 +0700

    Refactor test by removing local variable

--- test/captcha/security_test.clj
+++ test/captcha/security_test.clj
@@ -3,33 +3,25 @@
             [captcha.security :refer :all]))
 
 (deftest first-pattern-left-should-be-1
-  (def result (captcha 1 1 1 1))
-  (is (= "1" (get result :left))))
+  (is (= "1" (get (captcha 1 1 1 1) :left) )))
 
 (deftest first-pattern-left-should-be-9
-  (def result (captcha 1 9 1 1))
-  (is (= "9" (get result :left))))
+  (is (= "9" (get (captcha 1 9 1 1) :left) )))
 
 (deftest first-pattern-left-should-be-one
-  (def result (captcha 2 1 1 1))
-  (is (= "one" (get result :left))))
+  (is (= "one" (get (captcha 2 1 1 1) :left) )))
 
 (deftest first-pattern-left-should-be-nine
-  (def result (captcha 2 9 1 1))
-  (is (= "nine" (get result :left))))
+  (is (= "nine" (get (captcha 2 9 1 1) :left) )))
 
 (deftest first-pattern-right-should-be-1
-  (def result (captcha 1 1 1 1))
-  (is (= "1" (get result :right))))
+  (is (= "1" (get (captcha 1 1 1 1) :right))))
 
 (deftest first-pattern-right-should-be-9
-  (def result (captcha 1 1 1 9))
-  (is (= "9" (get result :right))))
+  (is (= "9" (get (captcha 1 1 1 9) :right))))
 
 (deftest first-pattern-right-should-be-one
-  (def result (captcha 2 1 1 1))
-  (is (= "one" (get result :right))))
+  (is (= "one" (get (captcha 2 1 1 1) :right))))
 
 (deftest first-pattern-right-should-be-nine
-  (def result (captcha 2 1 1 9))
-  (is (= "nine" (get result :right))))
+  (is (= "nine" (get (captcha 2 1 1 9) :right))))
