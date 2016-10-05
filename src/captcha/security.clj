ddd494e713417025a598e32971f54c4978a866d7
Twin Panichsombat <roofimon@gmail.com>
2016-10-05 12:12:39 +0700

    Extract methed for better abstraction

--- src/captcha/security.clj
+++ src/captcha/security.clj
@@ -2,28 +2,33 @@
 
 (def m ["one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
 
-(defn second?
-  [pattern]
-  (= pattern 2))
+(defn string [captcha]
+  (str (get captcha :left) " " (get captcha :operator) " " (get captcha :right)))
+
+(defn second? [pattern] (= pattern 2))
+
+(defn string-operand [index] (get m (- index 1)) )
+
+(def integer-operand (fn [index] (str index)))
 
 (defn assign-left-operand
   [pattern left]
   (if (second? pattern)
-    (get m (- left 1))
-    (str left) ))
+    (string-operand left)
+    (integer-operand left) ))
 
 (defn assign-right-operand
   [pattern right]
   (if (second? pattern)
-    (str right) )
-    (get m (- right 1)))
+    (integer-operand right)
+    (string-operand right) ))
 
 (defn assign-operator
   [operator]
-  (get {:1 "+" :2 "*" :3 "-"} (- operator 1)))
+  (get ["+" "*" "-"] (- operator 1)))
 
 (def captcha
     (fn [pattern left operator right]
       {:left (assign-left-operand pattern left)
         :operator (assign-operator operator)
-        :right (assign-left-operand pattern right)}))
+        :right (assign-right-operand pattern right)}))
--- test/captcha/security_test.clj
+++ test/captcha/security_test.clj
@@ -2,26 +2,32 @@
   (:require [clojure.test :refer :all]
             [captcha.security :refer :all]))
 
+(deftest with-first-pattern-captcha-should-be-1-plus-one
+  (is (= "1 + one" (string (captcha 1 1 1 1)))))
+
+(deftest with-second-pattern-captcha-should-be-one-plus-1
+  (is (= "one + 1" (string (captcha 2 1 1 1)))))
+
 (deftest first-pattern-left-should-be-1
   (is (= "1" (get (captcha 1 1 1 1) :left) )))
 
 (deftest first-pattern-left-should-be-9
   (is (= "9" (get (captcha 1 9 1 1) :left) )))
 
-(deftest first-pattern-left-should-be-one
+(deftest second-pattern-left-should-be-one
   (is (= "one" (get (captcha 2 1 1 1) :left) )))
 
-(deftest first-pattern-left-should-be-nine
+(deftest second-pattern-left-should-be-nine
   (is (= "nine" (get (captcha 2 9 1 1) :left) )))
 
-(deftest first-pattern-right-should-be-1
-  (is (= "1" (get (captcha 1 1 1 1) :right))))
-
-(deftest first-pattern-right-should-be-9
-  (is (= "9" (get (captcha 1 1 1 9) :right))))
-
 (deftest first-pattern-right-should-be-one
-  (is (= "one" (get (captcha 2 1 1 1) :right))))
+  (is (= "one" (get (captcha 1 1 1 1) :right))))
 
 (deftest first-pattern-right-should-be-nine
-  (is (= "nine" (get (captcha 2 1 1 9) :right))))
+  (is (= "nine" (get (captcha 1 1 1 9) :right))))
+
+(deftest second-pattern-right-should-be-1
+  (is (= "1" (get (captcha 2 1 1 1) :right))))
+
+(deftest second-pattern-right-should-be-9
+  (is (= "9" (get (captcha 2 1 1 9) :right))))
