5800f566d690376299326536328ee9eab7dabcec
Twin Panichsombat <roofimon@gmail.com>
2016-10-05 07:44:44 +0700

    Add auto refresh test

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot clojure-noob.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all} :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.17.0"]]}})
